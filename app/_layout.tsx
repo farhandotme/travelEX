3aaee3b694873df45606698c04d19df883080875
farhandotme <princeofficial355@gmail.com>
2025-12-22 21:08:00 +0530

    done with splash screen which is onboarding and added fonts to the project but only in splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
+import { useFonts } from 'expo-font';
+
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+
+  const [fontsLoaded] = useFonts({
+    StoryScript: require("../assets/fonts/StoryScript-Regular.ttf")
+  })
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
--- app/onBoarding.tsx
+++ app/onBoarding.tsx
@@ -1,38 +1,35 @@
-// import { LinearGradient } from "expo-linear-gradient";
-// import { useRouter } from "expo-router";
-// import { Image, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
+import { Image, Platform, Text, View } from "react-native";
 
 
-// export default function OnBoarding() {
-//   const router = useRouter();
-
-//   return (
-//     <LinearGradient colors={["#0171B1", "#001747"]} style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       <View><Text className="text-white text-5xl font-bold">Travel EX</Text> <Image source={require("../assets/images/logo.png")} /></View>
-//     </LinearGradient>
-//   );
-// }
-
-
-
-import { useRouter } from 'expo-router';
-import React, { useEffect } from 'react';
-import { Image } from 'react-native';
-
-const OnBoarding = () => {
-  const router = useRouter(); // ✅ hook at top level
+import { useEffect } from "react";
+import "../global.css";
+export default function OnBoarding() {
+  const router = useRouter();
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      router.replace("/(tabs)");
+    setTimeout(() => {
+      router.replace("/(tabs)")
     }, 3000);
+  })
 
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
-    <Image source={require("../assets/images/splashScreen.jpeg")} />
-  )
-}
+    <LinearGradient colors={["#0171B1", "#001747"]} style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View className="flex-row items-center ml-20 mr-20">
+        <Text style={{ fontFamily: "StoryScript" }} className={`${Platform.OS === "android" ? "text-5xl" : " text-7xl "} text-white font-bold mt-5`}>Travel EX</Text>
+        <Image style={{ height: 120, width: 120 }} source={require("../assets/images/logoRemove.png")} />
+      </View>
 
-export default OnBoarding
+      <View className="flex items-center">
+        <Text style={{ fontFamily: "StoryScript" }} className="text-white text-3xl">
+          Find Your Dream
+        </Text>
+        <Text style={{ fontFamily: "StoryScript" }} className="text-white text-3xl">
+          Destination With Us
+        </Text>
+      </View>
+    </LinearGradient>
+  );
+}
